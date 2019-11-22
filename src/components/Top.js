717d43c31497b383df72aeaf62debed188167eb9
Joel Velis <joelvelis94@gmail.com>
2019-11-22 12:03:53 -0500

    increased my name font size

--- src/components/Top.js
+++ src/components/Top.js
@@ -6,7 +6,7 @@ class Top extends React.Component {
             <div className='page-top'>
                 <div class='home-blurb'>
                     <p style={{color: '#64FFD9'}}>Hi, my name is</p>
-                    <h1 style={{color: '#ffffff'}}>Joel Velis.</h1>
+                    <h1 id='my-name' style={{color: '#ffffff'}}>Joel Velis.</h1>
                     <h1 style={{ color: '#8892B0'}}>I'm a thinker, and a builder.</h1>
 
                     <p>I am a software developer based in New York City who loves building web applications with a broader goal in mind. </p>
